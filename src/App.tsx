99a4abd4727512eff883093aa49849c2104a5d2d
David I. Jenei <jenei.david1994@gmail.com>
2024-01-24 19:21:43 +0100

    Remove resize

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { useState, useEffect } from 'react';
 import Main from './components/Main/Main';
 
 function App() {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <div>
-      <Main key={JSON.stringify(windowSize)} />
+      <Main />
     </div>
   );
 }
